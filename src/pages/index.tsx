5444308bda5aeb6619246d65586d7201b28bbcce
Fábio <fabiouds@hotmail.com>
2023-09-03 14:37:28 +0100

    asda

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
+import {Button} from "@chakra-ui/react"
 
-import {
-  ChakraProvider,
-  theme, Modal, useDisclosure,
-} from "@chakra-ui/react"
-import {Favorite} from "@/pages/Favorite";
-import Table from "@/pages/Table";
 export default function Home() {
-  const {isOpen, onOpen, onClose} = useDisclosure()
 
   return (
     <>
+<Button>asdsa</Button>
 
-
-          <Table onOpen2={onOpen}/>
-          <Favorite isOpen={isOpen} onClose={onClose}/>
+         
     </>
   )
 }
